61184be7816e9451d2ef34dbd884a520a346ad60
tykhoncode <tykhoncode@gmail.com>
2025-10-04 20:29:28 +0200

    🗃️ chore(backend): setup Express app and connect to MongoDB

---
+++ backend/src/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Node.js is running successfully again!");
+});
+
+async function startServer() {
+  console.log("Starting server");
+  try {
+    await mongoose.connect(process.env.DATABASE_URL!);
+    console.log("Connected to MongoDB");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error("Failed to start server:", err);
+    process.exit(1);
+  }
+}
+
+startServer();
