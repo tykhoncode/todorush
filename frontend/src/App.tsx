27adb37516174d1e847d767e6f86c7004711672b
tykhoncode <tykhoncode@gmail.com>
2025-10-06 15:32:17 +0200

    💄 style(ui): add base styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,11 @@
-import { useState } from "react";
 import "./App.css";
+import TodoPage from "./pages/TodoPage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>ToDo Rush</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <h1>ToDoRush</h1>
+      <TodoPage />
     </>
   );
 }
---
+++ frontend/src/pages/TodoPage.tsx
@@ -0,0 +1,18 @@
+function TodoPage() {
+  return (
+    <div>
+      <p>
+        <strong>Type your "ToDo's" and click +</strong>
+      </p>
+      <p>Learn Axios</p>
+      <p>Learn Redux</p>
+      <p>Learn React Router</p>
+      <p>
+        Learn Something With A Very Long Naming, Like So Long, It Is Hard To Fit
+        It In One Line
+      </p>
+    </div>
+  );
+}
+
+export default TodoPage;
